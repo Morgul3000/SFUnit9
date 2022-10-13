2580028b616861f09974a9008d5c0fcf8d495d30
Morgul3000 <shokuave3000@gmail.com>
2022-10-13 10:26:48 +0300

    Add project files.

---
+++ SFUnit9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SFUnit9
+{
+    internal class Program
+    {
+        static Students student;
+        static void Main(string[] args)
+        {
+            student = new Students();
+            student.studentsEvent += HandleSort;
+            student.GetArrayStudents(5);                     //Инициализация листа фамилий студентов
+              
+            try 
+            {
+               student.ReadNumber();
+            }
+            catch (ArgumentException) 
+            {
+                Console.WriteLine("Ошибка. Цифра должна быть 1 или 2.");
+            }
+        }
+        static void HandleSort(int number)                   //Реализация обработчика события
+        {
+            switch (number) 
+            {
+                case 1: student.GetSortArray();              //Отработка метода GetSortArray - сортировка (А - Я)
+                    break;
+                case 2: student.GetReversArray();            //Отработка метода GetReversArray - сортировка (Я - А)
+                    break ;
+            }
+        }
+    }
+}
---
+++ SFUnit9/Students.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFUnit9
+{
+    internal class Students
+    {
+        public delegate void StudentsDelegate(int number);
+        public event StudentsDelegate studentsEvent;
+
+        private List<string> nameList = new List<string>();
+
+        public void GetArrayStudents(int x)                       //Инициализация листа фамилий студентов
+        {
+            nameList = new List<string>(x);
+            int num = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                num++;
+                Console.WriteLine($"Введите фамилию студента номер {num}:");
+                nameList.Add(Console.ReadLine());
+            }
+            Console.WriteLine();
+        }
+
+        public void ReadNumber()                                  //Метод получения варианта сортировки листа
+        {
+            int number;
+
+            Console.WriteLine("Введите номер сортировки: 1(А-Я) или 2(Я-А).");
+
+            while (!int.TryParse(Console.ReadLine(), out number))        //Проверка на ввод числового значения
+            {
+                Console.WriteLine("Некорректные данные! Введите число 1 или 2.");
+            }
+
+            if (number != 1 && number != 2) throw new ArgumentException();        //Проверка на корректный ввод значения
+
+            studentsEvent?.Invoke(number);
+        }
+        public void GetSortArray()                                            //GetSortArray - сортировка (А - Я)
+        {
+            nameList.Sort();
+
+            Console.WriteLine();
+            Console.WriteLine("Сортировка (А - Я)");
+            foreach (string name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+        public void GetReversArray()                          //GetReversArray - сортировка (Я - А)
+        {
+            nameList.Sort();
+            nameList.Reverse();
+
+            Console.WriteLine();
+            Console.WriteLine("Сортировка (Я - А)");
+            foreach (string name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
